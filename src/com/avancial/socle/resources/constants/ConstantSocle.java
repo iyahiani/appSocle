14569796b8618c6df33b5face2462f93ce3b599f
Bruno LE GLOAHEC <bruno.legloahec@gmail.com>
2014-09-01 23:43:40 +0200

    Commentaires + modification projet

--- src/com/avancial/socle/resources/constants/ConstantSocle.java
+++ src/com/avancial/socle/resources/constants/ConstantSocle.java
@@ -1,22 +1,27 @@
 package com.avancial.socle.resources.constants;
 
-public enum ConstantSocle {BUNDLE_PATH("com.app.resources.internationalization.Message"),
-	MESSAGE_ERR_TRANSLATION_NOT_FOUND("erreur_traduction_non_trouve")
-	,NAVIGATION_ACCUEIL("/pages/public/accueil?faces-redirect=true")
-	,NAVIGATION_LOGIN("/pages/public/login?faces-redirect=true")
-	
+/**
+ * @author bruno
+ *
+ */
+public enum ConstantSocle {
+   /**
+ * 
+ */
+   BUNDLE_PATH("com.app.resources.internationalization.Message"), MESSAGE_ERR_TRANSLATION_NOT_FOUND("erreur_traduction_non_trouve"), NAVIGATION_ACCUEIL("/pages/public/accueil?faces-redirect=true"), NAVIGATION_LOGIN(
+         "/pages/public/login?faces-redirect=true")
 
-	;
-	
-	
-	private String constant="";
-	
-	private ConstantSocle(String pconstant) {
-		this.constant=pconstant;
-	}
-	
-	public String toString() {
-		return this.constant;
-	}
+   ;
+
+   private String constant = "";
+
+   private ConstantSocle(String pconstant) {
+      this.constant = pconstant;
+   }
+
+   @Override
+   public String toString() {
+      return this.constant;
+   }
 
 }
