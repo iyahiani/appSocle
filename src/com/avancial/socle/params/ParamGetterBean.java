1ec969f6afa9120f87d04b392c98086f74bf40f2
bruno.legloahec <bruno.legloahec@S4D38549.sysrail.local>
2015-01-09 16:27:21 +0100

    Parametres

--- src/com/avancial/socle/params/ParamGetterBean.java
+++ src/com/avancial/socle/params/ParamGetterBean.java
@@ -14,7 +14,7 @@ import com.avancial.socle.params.exception.ParamNotFoundException;
  * @author bruno
  *
  */
-public class FacadeParamGetter {
+public class ParamGetterBean {
    private Map<String, Map<String, IParamBean>> mapParamBean;
 
    /**
@@ -23,7 +23,7 @@ public class FacadeParamGetter {
     * @param listeParam
     * 
     */
-   public FacadeParamGetter() {
+   public ParamGetterBean() {
       this.mapParamBean = new HashMap<>();
    }
 
