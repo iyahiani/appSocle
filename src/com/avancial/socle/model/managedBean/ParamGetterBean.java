2198033c1b198265c94d3d595d47920c32e57a46
bruno.legloahec <bruno.legloahec@S4D38549.sysrail.local>
2015-01-09 17:37:13 +0100

    Les paramètres fichier fonctionnent !

--- src/com/avancial/socle/model/managedBean/ParamGetterBean.java
+++ src/com/avancial/socle/model/managedBean/ParamGetterBean.java
@@ -1,11 +1,18 @@
 /**
  * 
  */
-package com.avancial.socle.params;
+package com.avancial.socle.model.managedBean;
 
+import java.io.Serializable;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+import com.avancial.socle.params.IParamReader;
+import com.avancial.socle.params.ParamReaderFileGeneric;
 import com.avancial.socle.params.beans.IParamBean;
 import com.avancial.socle.params.exception.ParamCollectionNotLoadedException;
 import com.avancial.socle.params.exception.ParamNotFoundException;
@@ -14,17 +21,36 @@ import com.avancial.socle.params.exception.ParamNotFoundException;
  * @author bruno
  *
  */
-public class ParamGetterBean {
+@Named("paramGetterBean")
+@ApplicationScoped
+public class ParamGetterBean implements Serializable {
+   /**
+    * 
+    */
+   private static final long                    serialVersionUID = 1L;
    private Map<String, Map<String, IParamBean>> mapParamBean;
 
    /**
     * Initialise la structure qui va accueillir les paramBean
     * 
     * @param listeParam
+    * @throws Exception
     * 
     */
-   public ParamGetterBean() {
+   public ParamGetterBean() throws Exception {
       this.mapParamBean = new HashMap<>();
+      String filePath = "";
+      String WEBINF = "WEB-INF";
+      URL url = ParamGetterBean.class.getResource("ParamGetterBean.class");
+
+      String className = url.getFile();
+
+      filePath = className.substring(0, className.indexOf(WEBINF) + WEBINF.length());
+
+      // On instancie les Paramètres du socle
+      ParamReaderFileGeneric socle = new ParamReaderFileGeneric(filePath + "/classes/com/avancial/socle/resources/socle.properties");
+      socle.loadParams("socle");
+      this.add(socle);
    }
 
    /**
