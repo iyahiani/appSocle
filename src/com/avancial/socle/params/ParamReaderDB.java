7396e88cb52fe9a690b961c57f333ba7f8f61f83
bruno.legloahec <bruno.legloahec@S4D38549.sysrail.local>
2015-01-12 16:50:32 +0100

    Implémentation Récup Param en BDD

---
+++ src/com/avancial/socle/params/ParamReaderDB.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.avancial.socle.params;
+
+import com.avancial.socle.data.controller.dao.AbstractDao;
+
+/**
+ * @author bruno.legloahec
+ *
+ */
+public class ParamReaderDB extends AParamReader {
+   private AbstractDao dao;
+
+   /**
+    * Constructeur
+    */
+   public ParamReaderDB(AbstractDao dao) {
+      this.setDao(dao);
+   }
+
+   /*
+    * (non-Javadoc)
+    * 
+    * @see com.avancial.socle.params.IParamReader#loadParams(java.lang.String)
+    */
+   @Override
+   public void loadParams(String paramsName) throws Exception {
+      // TODO ou pas
+      dao.
+   }
+
+   /**
+    * @return the dao
+    */
+   public AbstractDao getDao() {
+      return this.dao;
+   }
+
+   /**
+    * @param dao
+    *           the dao to set
+    */
+   public void setDao(AbstractDao dao) {
+      this.dao = dao;
+   }
+
+}
