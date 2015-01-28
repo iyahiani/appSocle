ea5d1f40751d5a9e21132442cbcf4d94d904b6c3
Bruno LE GLOAHEC <bruno.legloahec@gmail.com>
2015-01-28 23:14:47 +0100

    Doc

--- src/com/avancial/app/model/managedBean/ParamGetterManagedBean.java
+++ src/com/avancial/app/model/managedBean/ParamGetterManagedBean.java
@@ -7,11 +7,15 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 
 import com.avancial.socle.params.AParamGetter;
+import com.avancial.socle.params.IParamReader;
+import com.avancial.socle.params.beans.IParamBean;
 
 /**
  * Classe implémentant un point unique de gestion des paramètres. Inspiré du
  * design patttern Facade. <br>
- * Voir{@link IParamBean, IParamReader}
+ * Les paramètres du socle sont chargés dans la classe abstraite. Pour ajouter
+ * ceux de l'application, il suffit de compléter le constructeur.<br/>
+ * Voir {@link IParamBean} , {@link IParamReader}
  * 
  * @author bruno
  *
