b9bf78050a75e573c496001195ed304f85376e99
fabien.cuaz <fabien.cuaz@orange.fr>
2015-05-28 13:43:12 +0200

    Refactoring Exceptions du socle

--- src/com/avancial/socle/exceptions/SocleExceptionManager.java
+++ src/com/avancial/socle/exceptions/SocleExceptionManager.java
@@ -4,17 +4,46 @@
 package com.avancial.socle.exceptions;
 
 /**
- * C'est le point d'entrée pour le client.
- * Il est chargé de contruire la chaine des Exception builder.
+ * C'est le point d'entrée pour le client. Il est chargé de contruire la chaine
+ * des Exception builder.
  * 
  * @author bruno.legloahec
  *
  */
 public class SocleExceptionManager {
+	protected static ASocleExceptionFinder finder = null;
+	private String test;
 
-   public static ASocleException getException(Exception e) {
+	public SocleExceptionManager(Exception e) {
+		this.init(e);
+	}
 
-      ISocleExceptionBuilder builder = new SocleExceptionBuilderSqlDuplicateId(null, e);
-      return builder.getSocleException();
-   }
+	@SuppressWarnings("static-method")
+	protected void init(Exception e) {
+		ASocleExceptionFinder finder = new SocleExceptionFinderSqlDuplicateId(null, e);
+
+		// A laisser à la fin. Doit pointer sur le dernier finder
+		SocleExceptionManager.finder = finder;
+	}
+	
+	
+	/**
+	 * 
+	 * @param finder
+	 */
+	public  static void add(ASocleExceptionFinder finder) {
+			SocleExceptionManager.finder=finder;
+	}
+
+	public static ASocleException getException() {
+		return SocleExceptionManager.finder.getSocleException();
+	}
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
 }
