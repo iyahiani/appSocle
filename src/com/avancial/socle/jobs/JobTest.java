09d1c8a621a938356fbb77bb375f9adbb2301b8d
Bruno LE GLOAHEC <bruno.legloahec@avancial.com>
2015-08-28 10:16:29 +0200

    Ajout des jobs Gestion des fontes

---
+++ src/com/avancial/socle/jobs/JobTest.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.avancial.socle.jobs;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ * @author bruno.legloahec
+ *
+ */
+public class JobTest implements Job {
+
+   /*
+    * (non-Javadoc)
+    * 
+    * @see org.quartz.Job#execute(org.quartz.JobExecutionContext)
+    */
+   @Override
+   public void execute(JobExecutionContext arg0) throws JobExecutionException {
+      System.out.println("Job executed !!");
+
+   }
+
+}
